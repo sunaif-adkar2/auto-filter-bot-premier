93c09c42fcd9e2f976880fe97f408d6a8411a54f
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-08-03 14:41:22 +0530

    Update motech.py

--- bot/motech.py
+++ bot/motech.py
@@ -1,20 +1,20 @@
-"""Running Code MoTechYT
-Editing By Pr0fess0r 99
-GNU Affero General Public License v3.0"""
-Permissions of this strongest copyleft license
+"""eunning Code moTechYT
+editing By Pr0fess0r 99
+gnu affero general public license v3.0
+permissions of this strongest copyleft license
 are conditioned on making available complete
 source code of licensed works and modifications,
 which include larger works using a licensed work,
 under the same license. Copyright and license
 notices must be preserved. Contributors provide
 an express grant of patent rights.
-When a modified version is used to provide a service
+when a modified version is used to provide a service
 over a network, the complete source code of the modified
 version must be made available.
 »https://github.com/AlbertEinsteinTG»https://github.com/MoTechYT»https://github.com/PR0FESS0R-99/Pro-Auto-Filter-Bot-V2
 your main Channel Added from Heroku
-Update
+update"""
 MT_BOT_UPDATES = "t.me/Mo_tech_YT" # eg
-Your Main Channel
+"""your Main Channel"""
 BOT_UPDATES = "t.me/Mo_Tech_YT" # eg
 
