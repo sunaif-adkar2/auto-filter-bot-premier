91cc438c166e48f6405093e2c4179b9250abd8a3
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-08-04 07:04:47 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -17,36 +17,17 @@ class Translation(object):
 <b>Press /help to know about available commands🤪</b>"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
+<b>🙄there is Google for help you!🙄?</b>
 
 <i>
--> sorry dude u can't help you 😆
+-> sorry dude i can't help you 😆
 ->  chodich poyavan oombi🤣
 </i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
+<b> PLEASE JOIN US FOR MOVIE @Movie_addictzs </b>
 
-    -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
     
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-            
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/sunaif_adkar">© sunaif adkar</a></b>
 """
     
     ABOUT_TEXT = """<b>➥ 📃Name</b> : <b> Auto Filter Bot</b>
